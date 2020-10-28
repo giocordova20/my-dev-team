e3fb78eb6d836a43d35e8bdd4558255dab6d02ee
giocode <giocordova@gmail.com>
2020-10-27 23:14:58 -0400

    removed console logs

--- app.js
+++ app.js
@@ -12,38 +12,13 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 const devTeam = []  // Array to hold all the team members
-const ids = []       // Array to hold IDs to maker sure they are not duplicated
+const ids = []      // Array to hold IDs to maker sure they are not duplicated
 
 function getTeamInfo () {
 
     //// Start with getting the Manger information then prompt user for the rest of the team. \\\\
     function managerInfo (){
-
         inquirer.prompt([
             {
                 type: "input",
@@ -93,11 +68,7 @@ function getTeamInfo () {
             }
         
         ]).then(answers => {
-            console.log("");
-            console.log("answers: ", answers);
-            log(chalk.bgGreen.white.bold("       >> Manger was created. << \n   >> Continue creating your team. <<"));
-            console.log("");
-            
+            log(chalk.bgGreen.white.bold("       >> Manger was created. <<     \n   >> Continue creating your team. <<"));
             const manager = new Manager (answers.mgrName, answers.mgrID, answers.mgrEmail, answers.mgrOffice);
             devTeam.push(manager);
 
@@ -119,10 +90,6 @@ function getTeamInfo () {
                 choices: ["Engineer", "Intern", "Team Completed"],
             }
         ]).then(answers => {
-            console.log("");
-            console.log("answers: ", answers);
-            console.log("");
-
             switch (answers.teamMember){
                 case "Engineer":
                     engineerInfo()
@@ -133,19 +100,12 @@ function getTeamInfo () {
                 case "Team Completed":
                     teamCompleted()
                     break;
-
             };
-
-
-
         });
-
-
     };
 
     //// Get Engineer Information \\\\
     function engineerInfo () {
-
         inquirer.prompt([
             {
                 type: "input",
@@ -197,23 +157,18 @@ function getTeamInfo () {
                     return true
                 }
             }
-        
         ]).then(answers => {
-            console.log("");
-            console.log("answers: ", answers);
-            log(chalk.bgGreen.white.bold("       >> Engineer was created. << \n     >> Continue creating your team. <<"));
-            console.log("");
+            log(chalk.bgGreen.white.bold("       >> Engineer was created. <<     \n     >> Continue creating your team. <<"));
             
             const engineer = new Engineer (answers.engrName, answers.engrID, answers.engrEmail, answers.engrGitHub);
             devTeam.push(engineer);
 
             // Get the Team
             getTeam();
-
         });
 
 
-        };
+    };
     
     //// Get Intern Information \\\\
     function internInfo () {
@@ -250,7 +205,6 @@ function getTeamInfo () {
                 message: chalk.bgBlue.white("What is your intern's email address?"),
                 validate: input => {
                     if (input === ""){
-                    // if (input === "" || parseInt(input)=== false){
                         log(chalk.magenta.bold("\n    >> Please enter a valid email address"));
                         return false
                     } 
@@ -263,20 +217,14 @@ function getTeamInfo () {
                 message: chalk.bgBlue.white("What is the intern's school?"),
                 validate: input => {
                     if (input === ""){
-                        log(chalk.magenta.bold("\n    >> Please enter a valid school"));
-                        return false
+                        return log(chalk.magenta.bold("\n    >> Please enter a valid school"));
                     } 
                     return true
                 }
             }
-        
         ]).then(answers => {
-            console.log("");
-            console.log("answers: ", answers);
-            log(chalk.bgGreen.white.bold("       >> Engineer was created. << \n     >> Continue creating your team. <<"));
-            console.log("");
+            log(chalk.bgGreen.white.bold("       >> Engineer was created. <<     \n     >> Continue creating your team. <<"));
             
-
             const intern = new Intern (answers.intrName, answers.intrID, answers.intrEmail, answers.intrSchool);
             devTeam.push(intern);
 
@@ -287,27 +235,19 @@ function getTeamInfo () {
         });
 
 
-        };
+    };
 
+    //// Write the HTML file in the 'output' folder
     function teamCompleted () {
-
-        console.log("devTeam");
-        console.log(devTeam);
-        console.log("devTeam");
-
         if(!fs.existsSync(OUTPUT_DIR)){
             fs.mkdirSync(OUTPUT_DIR);
         }
         fs.writeFileSync(outputPath, render(devTeam), (err) => {
             if (err) throw err;
-            log(chalk.bgGreen.white("File was created"))
         });
+        log(chalk.bgGreen.white("  HTML file was created in the output folder."))
+
     };
-
-
-
-
-
 
     managerInfo();
 
